edcaec94c15b5d98c03845f6f905386dfb9ce84e
MohammedAnisMultani <147980347+MohammedAnisMultani@users.noreply.github.com>
2024-01-25 13:57:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-//your JS code here. If required.
+h3{
+    text-align: center;
+    margin: 0px;
+    padding: 0px;
+}
+.table-1,
+.table-2,
+.table-3
+{
+    margin: auto;
+    width: 80%;
+}
+
+#t12,
+#t22,
+#t32
+{
+    text-align: center;
+}
+
+#t13,
+#t23,
+#t33
+{
+    text-align: right;
+}
+
+.table-2
+{
+   height: 120px;
+   margin-top: 50px;
+}
+.table-3
+{
+    height: 120px;
+   margin-top: 50px;
+}
+
+#image
+{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
